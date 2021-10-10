bf2f50d4c68f18b8574a9fbe3f6f3c4d504a643a
Matthew Karl Mostrom <mkmostro@bohr.chem.ncsu.edu>
2021-10-10 19:07:49 -0400

    problem set started

---
+++ SM2/problems/I5.jl
@@ -0,0 +1,31 @@
+import SpecialFunctions
+
+points = 1000
+ints = (points - 1)             #intervals between points, the things actually being integrated over
+x0 = 0
+x_min = 0
+x_max = 5
+dx = (x_max - x_min) / ints
+range = x_min:dx:x_max          # start:iteration:finish
+
+alpha = 0.5
+sigma = sqrt(2*alpha)
+
+g(x) = (1 / sqrt(2 * pi * sigma ^ 2)) * exp(-alpha * (x-x0) ^ 2)            # Change your function here
+#g(x) = k * exp(-(x-x0) ^ 2)
+
+lhr(f,range) = sum(f.(range[1:end-1]) .* step(range)) #f operating on each element of range and evaluating at each range
+rhr(f,range) = sum(f.(range[2:end])   .* step(range))
+
+println("")
+println("*************")
+println("Result from LHR is ", lhr(g,range))
+println("Result from RHR is ", rhr(g,range))
+
+av = (lhr(g,range) + rhr(g,range)) / 2
+println("Their average is ", av)
+println("")
+
+erf = SpecialFunctions.erf
+answer = 0.5 * sqrt(pi/alpha) * (erf(sqrt(alpha) * x_max) - erf(sqrt(alpha) * (x_min)))
+println("Analytical answer: ", answer) #https://www.wolframalpha.com/input/?i=Integrate%5Bexp%28-a*x%5E2%29%5D
---
+++ SM2/problems/I6.jl
@@ -0,0 +1,47 @@
+import SpecialFunctions
+points = 500000
+ints = (points - 1) #intervals between points, the things actually being integrated over
+x_min = 0.001
+x_max = 1000            #the error seems to go up with how big you make x_max
+dx = (x_max - x_min) / ints
+range = x_min:dx:x_max  # start:iteration:finish
+
+ge(x) = exp(-2 * x) / x
+gs(x) = sin(x) / x
+
+lhr(f,range) = sum(f.(range[1:end-1]) .* step(range)) #f operating on each element of range and evaluating at each range
+rhr(f,range) = sum(f.(range[2:end])   .* step(range))
+
+println("")
+println("____________________")
+println("")
+println("Result for Exp from LHR is ", lhr(ge,range))
+println("Result for Exp from RHR is ", rhr(ge,range))
+
+av = (lhr(ge,range) + rhr(ge,range)) / 2
+println("Trap rule average for Exp(-2x)/x is ", av)
+exp_answer = 5.63939        #Ei(x)
+println("Analytical Ei(x): ", exp_answer)
+println("")
+
+println("**")
+println("Result for Sin from LHR is ", lhr(gs,range))
+println("Result for Sin from RHR is ", rhr(gs,range))
+
+av = (lhr(gs,range) + rhr(gs,range)) / 2
+println("Trap rule average for sin(x)/x is ", av)
+sin_answer = 1.56923
+println("Analytical Si(x): ", sin_answer, " which is close to pi/2 (1.57090)")
+println("")
+
+
+matrix = zeros(2,length(range))
+f = open("I6_exp.dat","w")
+
+
+close(f)
+
+f = open("I6_sin.dat","w")
+
+
+close(f)
