43981fd76b9c893a0615867cbf5955ec5927e5ce
U-DESKTOP-CECAQLS\matth <matthewmostrom@gmail.com>
2021-10-20 23:21:37 -0400

    finished I6

--- SM2/problems/I4.jl
+++ SM2/problems/I4.jl
@@ -1,6 +1,8 @@
 import SpecialFunctions
 using Printf
 using DelimitedFiles
+
+array_range = 1:1000
 x0 = 0
 x_min = 0
 x_max = 5
@@ -10,7 +12,7 @@ println("")
 println("*******************")
 println("This program is an answer problem I4, and will output a 4-column file called 'I4_error.dat', which will graph rectangle rule and trapezoidal rule integration error against dx, respectively.")
 println("")
-@printf("First Moment: %f \nx-min: %f \nx-max: %f \nalpha : %f \nsigma : %f", x0,x_min,x_max,alpha,sigma)
+@printf("First Moment: %f \nx-min: %f \nx-max: %f \nalpha : %f \nSt. Dev. : +- %f", x0,x_min,x_max,alpha,sigma)
 println("")
 
 g(x) = (1 / sqrt(2 * pi * sigma^2)) * exp(-alpha * (x - x0)^2)            # Change your function here
@@ -20,7 +22,7 @@ lhr(f, range) = sum(f.(range[1:end-1]) .* step(range))
 rhr(f, range) = sum(f.(range[2:end]) .* step(range))
 
 
-array_range = 1:1000
+
 err_array = zeros(Float64,length(array_range),3)
 
 for i in array_range
@@ -41,10 +43,10 @@ for i in array_range
     err_array[i,2] = rect_err
     err_array[i,3] = trap_err
 
-    println("dx: ",dx)
-    println("rect_err: ",rect_err," %")
-    println("trap_err: ",trap_err," %")
-    println("")
+    #println("dx: ",dx)
+    #println("rect_err: ",rect_err," %")
+    #println("trap_err: ",trap_err," %")
+    #println("")
 end                 #i loop
 
 #print(err_array)
--- SM2/problems/I6.jl
+++ SM2/problems/I6.jl
@@ -1,14 +1,14 @@
 #using Pkg;Pkg.add("Plots");Pkg.add("PyPlot")
-
+using DelimitedFiles
 import SpecialFunctions
-points = 500000
+points = 50
 ints = (points - 1) #intervals between points, the things actually being integrated over
 x_min = 0.001
-x_max = 1000            #the error seems to go up with how big you make x_max
+x_max = 1000.            #the error seems to go up with how big you make x_max
 dx = (x_max - x_min) / ints
 range = x_min:dx:x_max  # start:iteration:finish
 
-ge(x) = exp(-2 * x) / x
+ge(x) = exp(-2. * x) / x
 gs(x) = sin(x) / x
 
 lhr(f,range) = sum(f.(range[1:end-1]) .* step(range)) #f operating on each element of range and evaluating at each range
@@ -20,7 +20,7 @@ println("")
 println("Result for Exp from LHR is ", lhr(ge,range))
 println("Result for Exp from RHR is ", rhr(ge,range))
 
-av = (lhr(ge,range) + rhr(ge,range)) / 2
+av = (lhr(ge,range) + rhr(ge,range)) / 2.
 println("Trap rule average for Exp(-2x)/x is ", av)
 exp_answer = 5.63939        #Ei(x) wolfram
 println("Analytical Ei(x): ", exp_answer)
@@ -37,9 +37,40 @@ println("Analytical Si(x): ", sin_answer, " which is close to pi/2 (1.57090)")
 println("")
 
 #Plot the functions
-I6_array = zeros(Float64,1,)
+I6_exp = zeros(Float64,length(range),2)
+I6_sin = zeros(Float64,length(range),2)
+plotter(f,range) = f.(range[1:end])   .* step(range)
 
-for i in range
-    
+plot_exp = plotter(ge,range)
+plot_sin = plotter(gs,range)
 
+for i in 1:points
+  I6_exp[i,1] = dx * i
+  I6_exp[i,2] = plot_exp[i]
+
+  I6_sin[i,1] = dx * i
+  I6_sin[i,2] = plot_sin[i]
 end
+
+println("Plotting exp function...")
+touch("I6_exp.dat")
+outfile = "I6_exp.dat"
+open(outfile, "w") do f
+  for i in I6_exp
+    println(f, i)
+  end
+end
+writedlm("I6_exp.dat",I6_exp)
+println("Done!")
+println("")
+
+println("Plotting sin function...")
+touch("I6_sin.dat")
+outfile = "I6_sin.dat"
+open(outfile, "w") do f
+  for i in I6_sin
+    println(f, i)
+  end
+end
+writedlm("I6_sin.dat",I6_sin)
+println("Done!")
