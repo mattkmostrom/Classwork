48b7576acfebfd1bcb35c8e78a1e2d8d22c8a9bb
Matthew Karl Mostrom <matthewmostrom@gmail.com>
2021-10-25 17:48:17 -0400

    verlet: harmonic function

--- SM2/verlet.jl
+++ SM2/verlet.jl
@@ -1,10 +1,15 @@
-x0 = 0            #enter initial postion in angstroms
-v0 = 3            #enter velocity in angstroms/ps
+import SpecialFunctions
+using Printf
+using DelimitedFiles
+x = 0
+x0 = 1.            #enter initial postion in angstroms
+vx = 0.
+v0 = 3.            #enter velocity in angstroms/ps
 omega = 7         #harmonic frequency
 time = 10         #integration time in (periods)
 red_mass = 2      #reduced mass in amu
 ener = 10         #total energy of the system
-npts = 5000       #number of integration points
+npts = 50       #number of integration points
 
 #total_energy(x0,v0,omega,rmass,ener) =
 red_mass=red_mass * 10. / 6.022169 / 1.380662 #amu to K ps A in units where k_b = 1
@@ -21,9 +26,32 @@ range = [1:dx:npts]
 println("Integration stepsize is: ", dx)
 println("")
 
-f = open("potential.dat","w")
 
+potential = zeros(Float64,npts,2)
 
+function harmonic(x,vx)
+  x = x0
+  rmass = red_mass
+  for i in range
+    vx=0.5*rmass*omega*omega*x*x
+    potential[i,1] = x
+    potential[i,2] = vx
+    println(x," ",vx)
+    x = x + dx
+  end
+end
+print(potential)
+println("")
+h = harmonic(x,vx)
 
-#write(f, whatever data)
-close(f)
+@show h
+
+touch("potential.dat")
+outfile = "potential.dat"
+open(outfile, "w") do f
+  for i in potential
+    println(f, i)
+  end
+end
+writedlm("potential","step_# energy")
+writedlm("potential.dat",potential)
