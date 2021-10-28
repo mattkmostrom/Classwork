5591102d64f79f9f36908c86cc6d1ed14b3a1bf2
Matthew Karl Mostrom <matthewmostrom@gmail.com>
2021-10-28 15:01:13 -0400

    works, phase is wrong

--- SM2/verlet.jl
+++ SM2/verlet.jl
@@ -2,18 +2,18 @@ import SpecialFunctions
 using Printf
 using DelimitedFiles
 x = 0
-x0 = 1.            #enter initial postion in angstroms
-vx = 0.
+x0 = 0.            #enter initial postion in angstroms
 v0 = 3.            #enter velocity in angstroms/ps
 t0 = 0
+k = 3
 omega = 7         #harmonic frequency
-time = 7         #integration time in (periods)
+time = 20         #integration time in (periods)
 red_mass = 2      #reduced mass in amu
 ener = 10         #total energy of the system
-npts = 50       #number of integration points
+npts = 5000       #number of integration points
 
 println("_________________________\n\n")
-println("Harmonic Oscillator MD v0.1\n")
+println("Harmonic Oscillator MD v1.0\n")
 
 #total_energy(x0,v0,omega,rmass,ener) =
 red_mass = red_mass * 10. / (6.022169 * 1.380662) #amu to K ps A in units where k_b = 1
@@ -21,42 +21,134 @@ fac = sqrt(2. * ener / (red_mass * omega ^ 2))
 xmax = fac
 xmin = -fac
 println("The classical turning points are +/-", xmax)
-println("")
-println("***************")
-
 println("Number of integration points/period: ", npts)
 dt = time / (npts - 1)
-range = [1:dx:npts]
-println("Integration stepsize is: ", dx)
+println("Timestep size: ",dt)
+
 println("")
 
-potential = zeros(Float64,npts,2)
-function harmonic(x)
+analytic = zeros(Float64,npts,2)
+euler = zeros(Float64,npts,2)
+verlet = zeros(Float64,npts,2)
+vel_verlet = zeros(Float64,npts,2)
+
+function harmonic(x,t)
   x = x0
+  time = t0
+
+  #harmonic analytic variables
+  w = sqrt(k/red_mass)
+  A = x0
+  B = v0 / w
+
+  #Euler variables
+	euler_x = 0.
+	euler_v = 0.
+	prev_euler_x = x0
+	prev_euler_v = v0
+
+	#Verlet variables
+	verlet_x = 0.
+	verlet_v = 0.
+	prev_verlet_x = x0
+
+	#velocity Verlet variables
+	prev_vel_verlet_x = 0.
+	prev_prev_verlet_x = x0
+	prev_vel_verlet_v = v0
+
   for i in 1:npts
-    vx = 0.5*red_mass*omega*omega*x*x
-    potential[i,1] = x
-    potential[i,2] = vx
+
+    #harmonic analytical answer ***************************************
+    harmonic_analytic = A*cos(w*time) + B*sin(w*time)
+    analytic[i,1] = time
+    analytic[i,2] = harmonic_analytic
+
+		#print(time," ",euler_x," \n")
+		#flush(stdout)
+
+    #Euler answer ***************************************
+		euler_x = prev_euler_x + prev_euler_v*dt
+		euler_v = prev_euler_v + (-w)*prev_euler_x*dt
+		prev_euler_x = euler_x
+		prev_euler_v = euler_v
+
+		euler[i,1] = time
+		euler[i,2] = euler_x
+
+    #Verlet answer ***************************************
+		verlet_x = 2*prev_verlet_x - prev_prev_verlet_x + (-w)*prev_verlet_x*dt*dt;
+		prev_prev_verlet_x = prev_verlet_x;
+		prev_verlet_x = verlet_x;
+
+		verlet[i,1] = time
+		verlet[i,2] = verlet_x
+
+    #Velocity Verlet answer ***************************************
+		vel_verlet_x = prev_vel_verlet_x + prev_vel_verlet_v*dt + 0.5*(-w)*prev_vel_verlet_x*dt*dt;
+		vel_verlet_v = prev_vel_verlet_v + 0.5*((-w)*vel_verlet_x + (-w)*prev_vel_verlet_x)*dt;
+		prev_vel_verlet_x = vel_verlet_x;
+		prev_vel_verlet_v = vel_verlet_v;
+
+		vel_verlet[i,1] = time
+		vel_verlet[i,2] = vel_verlet_x
+
+		#potential[i,1] = x
+    #potential[i,2] = vx
     #print(x," ",vx," ")
     #flush(stdout)
-    x = x + dx
+    time = time + dt
+    #x = x + dx
   end
 end
 
-println("Running dyanmics on harmonic oscillator...\n")
+println("***************")
+println("Running dyanmics...")
 
-h = harmonic(x0)
+h = harmonic(x0,time)
 
 println("... Done!\n")
-println("Writing harmonic results to 'potential.dat'...\n")
+println("Writing harmonic results to 'analytic.dat'...")
+println("Writing Euler results to 'euler.dat'...")
+println("Writing verlet results to 'verlet.dat'...")
+println("Writing velocity verlet results to 'vel_verlet.dat'...")
 
-touch("potential.dat")
-outfile = "potential.dat"
+touch("analytic.dat")
+outfile = "analytic.dat"
 open(outfile, "w") do f
-  for i in potential
+  for i in analytic
     println(f, i)
   end
 end
-writedlm("potential.dat",potential)
+writedlm("analytic.dat",analytic)
 
+touch("euler.dat")
+outfile = "euler.dat"
+open(outfile, "w") do f
+  for i in euler
+    println(f, i)
+  end
+end
+writedlm("euler.dat",euler)
+
+touch("verlet.dat")
+outfile = "verlet.dat"
+open(outfile, "w") do f
+  for i in verlet
+    println(f, i)
+  end
+end
+writedlm("verlet.dat",verlet)
+
+touch("vel_verlet.dat")
+outfile = "vel_verlet.dat"
+open(outfile, "w") do f
+  for i in vel_verlet
+    println(f, i)
+  end
+end
+writedlm("vel_verlet.dat",vel_verlet)
 println("... Done!")
+
+println("")
+println("xmgrace analytic.dat euler.dat verlet.dat vel_verlet.dat")
