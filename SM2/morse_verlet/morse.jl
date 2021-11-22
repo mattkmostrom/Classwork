b21dfac12a14c62c3e2955d784c70179d97cd499
Matthew Karl Mostrom <matthewmostrom@gmail.com>
2021-11-22 16:09:36 -0500

    array write problems

--- SM2/morse_verlet/morse.jl
+++ SM2/morse_verlet/morse.jl
@@ -1,94 +1,141 @@
+#To do:
+#1.) Make a box
+#2.)
+#
 import SpecialFunctions
+using LinearAlgebra
 using Printf
 using DelimitedFiles
-t0 = 0
-k = 3
-omega = 7         #harmonic frequency
-time = 20         #integration time in (periods)
-red_mass = 2      #reduced mass in amu
-ener = 10         #total energy of the system
-npts = 5000       #number of integration points
-dx = 3.
-
 println("________________________\n\n")
 println("Morse Potential MD v0.1\n")
 
-
-red_mass = red_mass * 10. / (6.022169 * 1.380662) #amu to K ps A in units where k_b = 1
-println("Number of integration points/period: ", npts)
-dt = time / (npts - 1)
+global nsteps = 5       #number of integration points
+global dt = 0.001
+println("Number of integration points/period: ", nsteps)
 println("Timestep size: ",dt)
-
 println("")
+
+
+mutable struct Element      #Make classes a thing in julia
+    type::String
+    mass::Real
+    eps::Real
+    sig::Real
+    w::Real
+    x0::Vector{<:Real}
+    pos::Vector{<:Real}
+    vel::Vector{<:Real}
+    acc::Vector{<:Real}
+    acc_old::Vector{<:Real}
+end
+
+Ar(x0::Vector,pos::Vector,vel::Vector,acc::Vector,acc_old::Vector) = Element("Ar",39.95,128.326802,3.371914,1,pos,pos,vel,acc,acc_old) #define some atom parameters
 
 #****************************
 #Make the cubic lattice
 #****************************
 
-struct Element      #Make classes a thing in julia
-    type::String
-    mass::Real
-    eps::Real
-    sig::Real
-    pos::Vector{<:Real}
-    vel::Vector{<:Real}
-end
-
-Ar(pos::Vector,vel::Vector) = Element("Ar",39.95,128.326802,3.371914,pos,vel) #define some atom parameters
-
-n = 5
-dx = 3
+n = 2
+dx = 3.
 atoms = []
 
-for i in 0:n    #Make a cubic lattice
-  for j in 0:n
-    for k in 0:n
-      
-      atom = Ar([i,j,k] .* dx,zeros(3)) #Ar is a function of 2 vectors: pos & vel 
-      #atom = Ar([i,j,k] .* dx,rand(3)) #again but random velocities
-
+for i in 1:n    #Make a cubic lattice
+  for j in 1:n
+    for k in 1:n
+      #atom = Ar([i,j,k] .* dx,zeros(3),zeros(3),zeros(3)) #Ar is a function of 2 vectors: pos & vel
+      atom = Ar([i,j,k] .* dx,[i,j,k] .* dx,rand(3),zeros(3),zeros(3)) #again but random velocities
       push!(atoms,atom)
     end #i
   end   #j
 end     #k
-#print(atoms)
+
 
 #****************************
 #Make the velocities gaussian
 #****************************
 
-for i in atoms    
-  itemp = 0
-  itemp = 
-
-
-
+#target_temp = 25
+function gaussify(atoms,target_temp)
+  for i in 1:length(atoms)
+    #target_temp = 25
+    itemp = 0
+    itemp *= atoms[i].mass * dot(atoms[i].vel,atoms[i].vel)
+    itemp /= 3 * length(atoms) - 3
+    atoms[i].vel *= sqrt(target_temp/itemp)
+  end
 end
 
 
+function forces(atoms)
+  for i in 1:length(atoms)
+    atoms[i].acc = -(atoms[i].w)^2 .* (atoms[i].pos - atoms[i].x0)    #calculate forces
+  end
+end
 
+print(atoms)
+traj = zeros(Float64,nsteps,3)
+function integrate(atoms,dt)
+  for j in 1:nsteps
+    posx = atoms[j].pos[1]
+    posy = atoms[j].pos[2]
+    posz = atoms[j].pos[3]
 
+    #print(posx,", ")
+    flush(stdout)
 
+    #traj[j][1] = posx
+    #traj[j][2] = posy
+    #traj[j][3] = posz
+
+    atoms[j].pos = atoms[j].pos + atoms[j].vel * dt + 0.5 * atoms[j].acc * dt^2   #Integrate
+    atoms[j].acc_old = atoms[j].acc
+
+    forces(atoms)         #Calculate forces
+
+    atoms[j].vel = atoms[j].vel + 0.5 * (atoms[j].acc + atoms[j].acc_old) * dt  #Update velocities
+  end
+end
+
+#****************************
+#Initialize some stuff
+#****************************
 
 println("")
 println("***************")
 println("Running dynamics...")
 
 
+gaussify(atoms,25)
+forces(atoms)
+
+
+#****************************
+#Write trajectories
+#****************************
+function write_output(filename,atoms)
+  touch("traj.dat")
+  outfile = "traj.dat"
+  open(outfile, "w") do f
+    for i in traj
+      println(f, i)
+    end
+  end
+  writedlm("traj.dat",traj)
+end
+
+
+
+
+
+#****************************
+#Run the dynamics
+#****************************
+
+for n in 1:nsteps
+  integrate(atoms,dt)
+
+end
+
+println("")
 println("... Done!\n")
-
-println("Writing Trajectory to 'out.dat'")
-
-#touch("out.dat")
-#outfile = "out.dat"
-#open(outfile, "w") do f
-#  for i in traj
-#    println(f, i)
-#  end
-#end
-#writedlm("out.dat",traj)
-
-println("... Done!")
-
-
 println("")
