424b73dde6da27a2536e6cddc6988c2e8cafe5ee
Matthew Mostrom <matthewmostrom@gmail.com>
2021-11-22 10:32:31 -0500

    further struct garbage

--- SM2/morse_verlet/morse.jl
+++ SM2/morse_verlet/morse.jl
@@ -21,68 +21,52 @@ println("Timestep size: ",dt)
 
 println("")
 
-abstract type Element end
+#****************************
+#Make the cubic lattice
+#****************************
 
-struct Ar <: Element 
+struct Element      #Make classes a thing in julia
     type::String
-    mass::Float64
-    eps::Float64
-    sig::Float64
-    pos_x::Float64
-    pos_y::Float64
-    pos_z::Float64
-    vel_x::Float64
-    vel_y::Float64
-    vel_z::Float64
+    mass::Real
+    eps::Real
+    sig::Real
+    pos::Vector{<:Real}
+    vel::Vector{<:Real}
 end
 
-struct He <: Element  
-    type::String
-    mass::Float64
-    eps::Float64
-    sig::Float64
-    pos_x::Float64
-    pos_y::Float64
-    pos_z::Float64
-    vel_x::Float64
-    vel_y::Float64
-    vel_z::Float64
-end
-
-type(c::Ar) = "Ar"
-mass(c::Ar) = "39.95"
-eps(c::Ar) = "128.326802"
-sig(c::Ar) = "3.371914"
-pos_x(c::Ar) = c.pos_x
-pos_y(c::Ar) = c.pos_y
-pos_z(c::Ar) = c.pos_z
-vel_x(c::Ar) = c.vel_x
-vel_y(c::Ar) = c.vel_y
-vel_z(c::Ar) = c.vel_z
+Ar(pos::Vector,vel::Vector) = Element("Ar",39.95,128.326802,3.371914,pos,vel) #define some atom parameters
 
 n = 5
-array_range = 1:1:n
-atoms = zeros(Float64,n,10)
-function cubic_lattice(n,dx)
+dx = 3
+atoms = []
 
-  for i in 1:n
-    for j in 1:n
-      for k in 1:n
-      atom = Ar(i*dx,j*dx,k*dx,0,0,0) 
-      print(atom)
-      flush(stdout)
-      append!(atoms,atom)
-      end #i
-    end   #j
-  end     #k
-  return atoms
-  #print(atoms)
-  #flush(stdout)
-end       #function
+for i in 0:n    #Make a cubic lattice
+  for j in 0:n
+    for k in 0:n
+      
+      atom = Ar([i,j,k] .* dx,zeros(3)) #Ar is a function of 2 vectors: pos & vel 
+      #atom = Ar([i,j,k] .* dx,rand(3)) #again but random velocities
 
-cube = cubic_lattice(5,3)
+      push!(atoms,atom)
+    end #i
+  end   #j
+end     #k
+#print(atoms)
 
-print(atoms)
+#****************************
+#Make the velocities gaussian
+#****************************
+
+for i in atoms    
+  itemp = 0
+  itemp = 
+
+
+
+end
+
+
+
 
 
 
