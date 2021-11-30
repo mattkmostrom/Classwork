9839c5db2b97df1b8a0718599e52f31e29d4a15c
Matthew Karl Mostrom <matthewmostrom@gmail.com>
2021-11-30 16:26:56 -0500

    dynamics works for any first der.

--- SM2/morse_verlet/morse.jl
+++ SM2/morse_verlet/morse.jl
@@ -7,14 +7,15 @@ using LinearAlgebra
 using Printf
 using DelimitedFiles
 println("________________________\n\n")
-println("Morse Potential MD v0.2\n")
+println("Morse Potential MD v0.9\n")
 
-global nsteps = 1000                 #number of integration points
-global cell = 10000.                    #Cell size
-global dt = 0.01                  #timestep size
-global n = 2                        #number of atoms to be simulated
-T = 300.                               #Temperature
-dx = 3.5                            #spacing between adjacent atoms in cube
+global kb = 1.38064852e-23
+global nsteps = 1500                #number of integration points
+global dt = 0.01                    #timestep size
+global n = 3                        #cube root of the number of atoms to be simulated
+global dx = 8.                      #spacing between adjacent atoms in cube
+
+
 println("Number of steps: ", nsteps)
 println("Timestep size: ",dt)
 println("")
@@ -22,11 +23,20 @@ println("")
 #****************************
 #Make the units correct
 #****************************
-#tstar =
-#massstar =                    #twisting your mind and smashing your dreams
-#rhostar =
+Tstar = 0.02                 #Temperature
+rhostar = 0.6               #density
+massstar = 32.              #twisting your mind and smashing your dreams
+eps = 1280.                  #well-depth
+sig = 3.405                 #place where E = 0, NOT x corresponding to bottom of well
+w = 1.                      #arbitrary potential-specific parameter; harmonic freq, morse alpha, etc
 
+T = Tstar * (kb/eps)
+mass = massstar * (10. / (6.022169 * 1.380662))
+rho = rhostar * (sig)^3
+#force = force * (atoms[1].sig/atoms[1].eps)
 
+cell = 0.                           #Cell size
+cell = (n ^ 3 / rho) ^ (1. / 3.)    #reduced units
 
 
 mutable struct Element      #Make classes a thing in julia
@@ -40,12 +50,12 @@ mutable struct Element      #Make classes a thing in julia
     vel::Vector{<:Real}
     acc::Vector{<:Real}
     acc_old::Vector{<:Real}
-end
+end#Cell size
 #alpha = 1                  #well width. bigger alpha means gentler sloped well. "bond stiffness"
 
 #He(x0::Vector,pos::Vector,vel::Vector,acc::Vector,acc_old::Vector) = Element("He",mass,eps,sigma,w,x0,pos,vel,acc,acc_old)
-#Ar(x0::Vector,pos::Vector,vel::Vector,acc::Vector,acc_old::Vector) = Element("Ar",39.95,128.326802,3.371914,1,x0,pos,vel,acc,acc_old) #define some atom parameters
-Ar(x0::Vector,pos::Vector,vel::Vector,acc::Vector,acc_old::Vector) = Element("Ar",39.95,1000,3.05,1,x0,pos,vel,acc,acc_old) #define some atom parameters
+#Ar(x0::Vector,pos::Vector,vel::Vector,acc::Vector,acc_old::Vector) = Element("Ar",39.95,128.326802,3.371914,1,x0,pos,vel,acc,acc_old) #LJ UFF atom parameters
+Ar(x0::Vector,pos::Vector,vel::Vector,acc::Vector,acc_old::Vector) = Element("Ar",mass,eps,sig,1,x0,pos,vel,acc,acc_old) #toy parameters
 
 
 #****************************
@@ -56,16 +66,24 @@ for i in 1:n
   for j in 1:n
     for k in 1:n
       x0 = [i,j,k] .* dx
-      atom = Ar(x0,x0,rand(3),zeros(3),zeros(3)) #Ar is a function of 5 vectors: x0,pos,vel,acc,acc_old
+      atom = Ar(x0,x0,zeros(3),zeros(3),zeros(3)) #Ar is a function of 5 vectors: x0,pos,vel,acc,acc_old
       push!(atoms,atom)
     end #i
   end   #j
 end     #k
 
+
+
+
+
 #****************************
 #Make the velocities gaussian
 #****************************
 function temper(atoms,target_temp)
+  for i in 1:length(atoms)
+      atoms[i].vel += randn(3)
+  end
+
   itemp = 0
   for i in 1:size(atoms,1)
     atoms[i].vel[1] -= 0.5
@@ -75,10 +93,11 @@ function temper(atoms,target_temp)
   end
 
   itemp /= 3 * size(atoms,1) - 3      #Need to comment out the '-3' if there's only 1 atom
-  for i in size(atoms,1)
+  for i in 1:size(atoms,1)
     atoms[i].vel *= sqrt(target_temp/itemp)
   end
 end
+
 
 #****************************
 #Take out Center of Mass velocities
@@ -107,18 +126,24 @@ function com(atoms)
       end
 end
 
+
 #****************************
 #Calculate forces (update potentials)
 #****************************
 function forces(atoms)      #calculate forces
   force = [0.,0.,0.]
+
+  for i in 1:length(atoms)
+    atoms[i].acc = zeros(3)
+  end
+
   for i in 1:size(atoms,1)
     for j in 1:size(atoms,1)
       #if i < j
       if atoms[i] != atoms[j]   #should this be i < j?
 
         d_pos = atoms[i].pos - atoms[j].pos    #Float64 #[dx,dy,dz]
-        #d_pos = d_pos .- (round.(Int64,( d_pos / cell )) * cell)      #[dx,dy,dz] - (Int64 * Float64)
+        d_pos = d_pos - (round.(Int64,( d_pos / cell )) * cell)      #[dx,dy,dz] - (Int64 * Float64)
 
         dr = sqrt(dot(d_pos,d_pos))     #sqrt(dx^2 + dy^2 + dz^2)   Float64
         d_pos = d_pos/dr                #[dx/dr, dy/dr , dz/dr]
@@ -134,13 +159,15 @@ function forces(atoms)      #calculate forces
         #Morse
         #******************************
         D = atoms[i].eps
-        alpha = 10                  #well width. bigger alpha means gentler sloped well. "bond stiffness"
-        expar = exp(- alpha * (dr .- atoms[i].sig))       #need to use dx_x0?
-        dudr = 2.0 * D * alpha * expar * (1.0 - expar)   #U = D * (1.0 - expar) * (1.0 - expar)
+        alpha = 1.                  #well width. bigger alpha means gentler sloped well. "bond stiffness"
+        expar = exp(- alpha .* (dr .- atoms[i].sig))       #need to use dx_x0?
+        dudr = (2.0 * D * alpha) .* expar .* (1.0 - expar)   #U = D * (1.0 - expar) * (1.0 - expar)
 
-        force[1] = (- dudr) * (d_pos[1]/dr)    #(dU/dr)*(dr/dx)
-        force[2] = (- dudr) * (d_pos[2]/dr)
-        force[3] = (- dudr) * (d_pos[3]/dr)
+        force[1] = (-dudr) * (d_pos[1]/dr)    #(dU/dr)*(dr/dx)
+        force[2] = (-dudr) * (d_pos[2]/dr)
+        force[3] = (-dudr) * (d_pos[3]/dr)
+
+        force = force .* (atoms[1].sig/atoms[1].eps)
 
         atoms[i].acc += force / atoms[i].mass             #a = F/m
       end
@@ -188,17 +215,18 @@ end
 
 function integrate(atoms,dt)
   global traj = zeros(Float64,size(atoms,1),3)
-  for j in 1:size(atoms,1)
+  for j in 1:length(atoms)
+  #for j in 1:size(atoms,1)
     traj[j,1] = atoms[j].pos[1]
     traj[j,2] = atoms[j].pos[2]
     traj[j,3] = atoms[j].pos[3]
 
-    atoms[j].pos = atoms[j].pos + (atoms[j].vel * dt) + (0.5 * atoms[j].acc * (dt^2))   #Integrate
+    atoms[j].pos = atoms[j].pos .+ atoms[j].vel * dt .+ (0.5 .* atoms[j].acc * (dt^2.))   #Integrate
     atoms[j].acc_old = atoms[j].acc
 
     forces(atoms)         #Calculate forces
 
-    atoms[j].vel = atoms[j].vel + (0.5 * (atoms[j].acc + atoms[j].acc_old) * dt)  #Update velocities
+    atoms[j].vel = atoms[j].vel .+ (0.5 .* (atoms[j].acc + atoms[j].acc_old) * dt)  #Update velocities
   end
   return traj
 end
@@ -208,8 +236,8 @@ end
 #Write trajectories
 #****************************
 function write_output(traj)
-  float_natoms = n^3
-  natoms = Int.(float_natoms)
+  natoms = n^3
+#  natoms = Int.(float_natoms)
   outfile = "traj.xyz"
   open(outfile, "a") do f
     @printf(f,"%i\n\n",natoms)
