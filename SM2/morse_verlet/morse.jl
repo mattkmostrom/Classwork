bd4b4f07575062b5b5e8fecef791210fbd943b07
Matthew Mostrom <matthewmostrom@gmail.com>
2021-11-30 00:52:31 -0500

    dynamics 'works', hugely repulsive

--- SM2/morse_verlet/morse.jl
+++ SM2/morse_verlet/morse.jl
@@ -9,15 +9,24 @@ using DelimitedFiles
 println("________________________\n\n")
 println("Morse Potential MD v0.2\n")
 
-global nsteps = 300       #number of integration points
-global cell = 10
-global dt = 0.001         #timestep size
-global n = 2              #number of atoms to be simulated
-T = 3
-dx = 3.1                   #spacing between adjacent atoms in cube
+global nsteps = 1000                 #number of integration points
+global cell = 10000.                    #Cell size
+global dt = 0.01                  #timestep size
+global n = 2                        #number of atoms to be simulated
+T = 300.                               #Temperature
+dx = 3.5                            #spacing between adjacent atoms in cube
 println("Number of steps: ", nsteps)
 println("Timestep size: ",dt)
 println("")
+
+#****************************
+#Make the units correct
+#****************************
+#tstar =
+#massstar =                    #twisting your mind and smashing your dreams
+#rhostar =
+
+
 
 
 mutable struct Element      #Make classes a thing in julia
@@ -36,18 +45,17 @@ end
 
 #He(x0::Vector,pos::Vector,vel::Vector,acc::Vector,acc_old::Vector) = Element("He",mass,eps,sigma,w,x0,pos,vel,acc,acc_old)
 #Ar(x0::Vector,pos::Vector,vel::Vector,acc::Vector,acc_old::Vector) = Element("Ar",39.95,128.326802,3.371914,1,x0,pos,vel,acc,acc_old) #define some atom parameters
-Ar(x0::Vector,pos::Vector,vel::Vector,acc::Vector,acc_old::Vector) = Element("Ar",39.95,50,3,1,x0,pos,vel,acc,acc_old) #define some atom parameters
+Ar(x0::Vector,pos::Vector,vel::Vector,acc::Vector,acc_old::Vector) = Element("Ar",39.95,1000,3.05,1,x0,pos,vel,acc,acc_old) #define some atom parameters
 
 
 #****************************
 #Make the cubic lattice
 #****************************
 atoms = []
-for i in 1:n    #Make a cubic lattice
+for i in 1:n
   for j in 1:n
     for k in 1:n
       x0 = [i,j,k] .* dx
-      #atom = Ar([i,j,k] .* dx,[i,j,k] .* dx,rand(3),zeros(3),zeros(3))
       atom = Ar(x0,x0,rand(3),zeros(3),zeros(3)) #Ar is a function of 5 vectors: x0,pos,vel,acc,acc_old
       push!(atoms,atom)
     end #i
@@ -57,17 +65,17 @@ end     #k
 #****************************
 #Make the velocities gaussian
 #****************************
-function gaussify(atoms,target_temp)
+function temper(atoms,target_temp)
   itemp = 0
-  for i in 1:length(atoms)
+  for i in 1:size(atoms,1)
     atoms[i].vel[1] -= 0.5
     atoms[i].vel[2] -= 0.5
     atoms[i].vel[3] -= 0.5
     itemp += atoms[i].mass * dot(atoms[i].vel,atoms[i].vel)
   end
 
-  itemp /= 3 * length(atoms) - 3      #Need to comment out the '-3' if there's only 1 atom
-  for i in length(atoms)
+  itemp /= 3 * size(atoms,1) - 3      #Need to comment out the '-3' if there's only 1 atom
+  for i in size(atoms,1)
     atoms[i].vel *= sqrt(target_temp/itemp)
   end
 end
@@ -76,25 +84,26 @@ end
 #Take out Center of Mass velocities
 #****************************
 function com(atoms)
-      sumvx= 0.
-      sumvy= 0.
-      sumvz= 0.
-      for i in 1:length(atoms)
-        sumvx=atoms[i].vel[1]+sumvx
-        sumvy=atoms[i].vel[2]+sumvy
-        sumvz=atoms[i].vel[3]+sumvz
+      sumvx = 0.
+      sumvy = 0.
+      sumvz = 0.
+
+      for i in 1:size(atoms,1)
+        sumvx = atoms[i].vel[1] + sumvx
+        sumvy = atoms[i].vel[2] + sumvy
+        sumvz = atoms[i].vel[3] + sumvz
       end
 
       #calculate the center of mass velocity
-      sumvx=sumvx/length(atoms)
-      sumvy=sumvy/length(atoms)
-      sumvz=sumvz/length(atoms)
+      sumvx = sumvx / size(atoms,1)
+      sumvy = sumvy / size(atoms,1)
+      sumvz = sumvz / size(atoms,1)
 
        #subtract off the center of mass velocity
-      for i in 1:length(atoms)
-         atoms[i].vel[1]=atoms[i].vel[1]-sumvx
-         atoms[i].vel[2]=atoms[i].vel[2]-sumvy
-         atoms[i].vel[3]=atoms[i].vel[3]-sumvz
+      for i in 1:size(atoms,1)
+         atoms[i].vel[1] = atoms[i].vel[1] - sumvx
+         atoms[i].vel[2] = atoms[i].vel[2] - sumvy
+         atoms[i].vel[3] = atoms[i].vel[3] - sumvz
       end
 end
 
@@ -102,41 +111,36 @@ end
 #Calculate forces (update potentials)
 #****************************
 function forces(atoms)      #calculate forces
-  for i in 1:length(atoms)
-    for j in 1:length(atoms)
-      if atoms[i] != atoms[j]
+  force = [0.,0.,0.]
+  for i in 1:size(atoms,1)
+    for j in 1:size(atoms,1)
+      #if i < j
+      if atoms[i] != atoms[j]   #should this be i < j?
 
-        dx = atoms[i].pos[1] - atoms[j].pos[1]
-        dy = atoms[i].pos[2] - atoms[j].pos[2]
-        dz = atoms[i].pos[3] - atoms[j].pos[3]
+        d_pos = atoms[i].pos - atoms[j].pos    #Float64 #[dx,dy,dz]
+        #d_pos = d_pos .- (round.(Int64,( d_pos / cell )) * cell)      #[dx,dy,dz] - (Int64 * Float64)
 
-        dx = dx - round(Int64,(dx/cell))*cell
-        dy = dy - round(Int64,(dy/cell))*cell
-        dx = dz - round(Int64,(dz/cell))*cell
+        dr = sqrt(dot(d_pos,d_pos))     #sqrt(dx^2 + dy^2 + dz^2)   Float64
+        d_pos = d_pos/dr                #[dx/dr, dy/dr , dz/dr]
 
-        rx = dot(dx,dx)
-        ry = dot(dy,dy)
-        rz = dot(dz,dz)
-        r = sqrt(rx + ry + rz)
-
-        dx = dx / r
-        dy = dy / r
-        dz = dz / r
-
+        #******************************
         #Lennard-Jones
-        #sig_r = atoms[i].sig / r
-        #U = 4 * atoms[i].eps * (((sig_r)^12)-((sig_r)^6))
-        #force = 24. * atoms[i].eps / (r ^ 2) * ( 2 * (atoms[i].sig / r) ^ 12 - (atoms[i].sig / r)^6 )
+        #******************************
+#        force = 24. * atoms[i].eps / (r ^ 2) * ( 2 * (atoms[i].sig / r) ^ 12 - (atoms[i].sig / r)^6 )
 
-        #FIXME: write if statement to choose potential type
+#        FIXME: write if statement to choose potential type
 
+        #******************************
         #Morse
+        #******************************
         D = atoms[i].eps
-        dr = r - atoms[i].sig
-        expar = exp(- atoms[i].w * dr)
-        U = D * (1.0 - expar) * (1.0 - expar)
-        dudr = 2.0 * D * atoms[i].w * expar * (1.0 - expar)   #U = D * (1.0 - expar) * (1.0 - expar)
-        force = (dudr / r) * dx
+        alpha = 10                  #well width. bigger alpha means gentler sloped well. "bond stiffness"
+        expar = exp(- alpha * (dr .- atoms[i].sig))       #need to use dx_x0?
+        dudr = 2.0 * D * alpha * expar * (1.0 - expar)   #U = D * (1.0 - expar) * (1.0 - expar)
+
+        force[1] = (- dudr) * (d_pos[1]/dr)    #(dU/dr)*(dr/dx)
+        force[2] = (- dudr) * (d_pos[2]/dr)
+        force[3] = (- dudr) * (d_pos[3]/dr)
 
         atoms[i].acc += force / atoms[i].mass             #a = F/m
       end
@@ -144,12 +148,14 @@ function forces(atoms)      #calculate forces
   end
 end
 
+
 function get_energy(atoms)
   total_vel = 0
   global energy = 0
-  for i in 1:length(atoms)
-    for j in 1:length(atoms)
+  for i in 1:size(atoms,1)
+    for j in 1:size(atoms,1)
       if atoms[i] != atoms[j]
+
         dx = atoms[i].pos - atoms[j].pos
         r = sqrt(dot(dx,dx))
         dx = dx / r
@@ -163,7 +169,7 @@ function get_energy(atoms)
         #Morse
         D = atoms[i].eps
         dr = r - atoms[i].sig
-        alpha = 1                  #well width. bigger alpha means gentler sloped well. "bond stiffness"
+        alpha = 2                  #well width. bigger alpha means gentler sloped well. "bond stiffness"
         expar = exp(- alpha * dr)
         U = D * (1.0 - expar) * (1.0 - expar)
 
@@ -180,25 +186,19 @@ function get_energy(atoms)
   end
 end
 
-#print(atoms)
-#println("")
 function integrate(atoms,dt)
-  global traj = zeros(Float64,length(atoms),3)
-  for j in 1:length(atoms)
+  global traj = zeros(Float64,size(atoms,1),3)
+  for j in 1:size(atoms,1)
     traj[j,1] = atoms[j].pos[1]
     traj[j,2] = atoms[j].pos[2]
     traj[j,3] = atoms[j].pos[3]
-    #println("")
-    #println("")
-    #print(traj)
 
-    atoms[j].pos = atoms[j].pos + atoms[j].vel * dt + 0.5 * atoms[j].acc * dt^2   #Integrate
+    atoms[j].pos = atoms[j].pos + (atoms[j].vel * dt) + (0.5 * atoms[j].acc * (dt^2))   #Integrate
     atoms[j].acc_old = atoms[j].acc
 
     forces(atoms)         #Calculate forces
 
-    atoms[j].vel = atoms[j].vel + 0.5 * (atoms[j].acc + atoms[j].acc_old) * dt  #Update velocities
-
+    atoms[j].vel = atoms[j].vel + (0.5 * (atoms[j].acc + atoms[j].acc_old) * dt)  #Update velocities
   end
   return traj
 end
@@ -225,8 +225,8 @@ end
 println("")
 println("***************")
 println("Running dynamics...")
-gaussify(atoms,T)
-com(atoms)
+temper(atoms,T)
+#com(atoms)
 forces(atoms)
 
 
